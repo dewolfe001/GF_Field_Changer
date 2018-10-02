5f0d2c954545d1f5d26dd1cc63689fb230a723cd
Shawn DeWolfe <shawn@shawndewolfe.com>
2018-10-01 19:13:33 -0700

    Create fieldchanger.js

---
+++ js/fieldchanger.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+	console.log("ready - image.js");
+});
+
+function SetFieldChanger(fieldChanger){
+	console.log("line 15 - " + fieldChanger.attr("id"));
+	/* TODO - real work */
+}
